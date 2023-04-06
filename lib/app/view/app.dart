34680e3cf83165590c096c362a4e6bccbaaf06cb
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2023-04-05 20:46:15 -0500

    Cambio a FluentUI

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,19 +1,15 @@
 import 'package:feeling_analysis/counter/counter.dart';
 import 'package:feeling_analysis/l10n/l10n.dart';
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
-        ),
-      ),
+    return FluentApp(
+      debugShowCheckedModeBanner: false,
+      theme: FluentThemeData.dark(),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       home: const CounterPage(),
--- lib/main_staging.dart
+++
@@ -1,6 +0,0 @@
-import 'package:feeling_analysis/app/app.dart';
-import 'package:feeling_analysis/bootstrap.dart';
-
-void main() {
-  bootstrap(() => const App());
-}
